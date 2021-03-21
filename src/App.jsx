7bc3bb0577ecae7bff1278edc4166e33949f3e89
Velidoss <godofthemountain09@gmail.com>
2021-03-21 17:01:25 +0200

    feat: created initial layout with eslint

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const a = 2;
+
+  return (
+    <div className="App">
+      hello, world!
+      {' '}
+      {a}
+    </div>
+  );
+}
+
+export default App;
