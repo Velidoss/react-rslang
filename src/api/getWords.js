0cf0ac3ca842e29af367c48ce478377ba06e2029
Velidoss <godofthemountain09@gmail.com>
2021-03-27 18:18:18 +0200

    refactor: removed console.log

--- src/api/getWords.js
+++ src/api/getWords.js
@@ -3,7 +3,6 @@ import createInstance from './createInstance';
 const getWords = async () => {
   const request = createInstance('/words', { group: 1, page: 10 });
   const response = await request.get();
-  console.log(response.data);
   return response.data;
 };
 
