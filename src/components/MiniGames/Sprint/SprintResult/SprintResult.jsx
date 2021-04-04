de37863243df7914b36a53fba6124e4d468ed624
Irina Stepanova <irina.s.vesna@mail.ru>
2021-04-04 19:44:36 +0600

    feat: add some styles for sprint-result

--- src/components/MiniGames/Sprint/SprintResult/SprintResult.jsx
+++ src/components/MiniGames/Sprint/SprintResult/SprintResult.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
 import PropTypes, { arrayOf } from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Button, Typography, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  sprintResultBtn: {
+    margin: '1rem',
+  },
+  sprintResultP: {
+    textAlign: 'center',
+  },
+  sprintResultMT: {
+    marginTop: '1rem',
+  },
+  sprintResultHr: {
+    margin: '1rem',
+  },
+}));
 
 const SprintResult = ({ answersState, points, startGame }) => {
   const calcRes = (r = 0, w = 0) => ((r / (r + w)) * 100) || 0;
   const percentage = calcRes(answersState.right.length, answersState.wrong.length).toFixed(0);
 
+  const classes = useStyles();
+
   return (
-    <div>
-      <div>{`Верные ответы: ${answersState.right.join(', ') || 'нет'}.`}</div>
-      <div>{`Неверные ответы: ${answersState.wrong.join(', ') || 'нет'}.`}</div>
-      <div>{`Итого: ${percentage} / 100%.`}</div>
-      <div>{`Очки: ${points}.`}</div>
-      <NavLink to="/minigames">
-        <Button>
-          Другие мини-игры
+    <>
+      <Typography className={`${classes.sprintResultMT} ${classes.sprintResultP}`}>
+        <span>Верные ответы: </span>
+        <span>{`${answersState.right.join(', ') || 'нет'}.`}</span>
+      </Typography>
+      <Typography className={classes.sprintResultP}>
+        <span>Неверные ответы: </span>
+        <span>{`${answersState.wrong.join(', ') || 'нет'}.`}</span>
+      </Typography>
+      <Divider className={classes.sprintResultHr} />
+      <Typography className={classes.sprintResultP}>
+        <span>Итого: </span>
+        <span>{`${percentage} / 100%.`}</span>
+      </Typography>
+      <Typography className={classes.sprintResultP}>
+        <span>Очки: </span>
+        <span>{`${points}.`}</span>
+      </Typography>
+      <Typography className={classes.sprintResultP}>
+        <NavLink to="/minigames">
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.sprintResultBtn}
+          >
+            Другие мини-игры
+          </Button>
+        </NavLink>
+        <Button
+          onClick={startGame}
+          variant="contained"
+          color="secondary"
+          className={classes.sprintResultBtn}
+        >
+          Сыграть еще раз
         </Button>
-      </NavLink>
-      <Button onClick={startGame}>
-        Сыграть еще раз
-      </Button>
-    </div>
+      </Typography>
+    </>
   );
 };
 
