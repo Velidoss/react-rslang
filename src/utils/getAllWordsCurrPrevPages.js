d395d977c178e1903e8d38555f912862bc4b8d57
Irina Stepanova <irina.s.vesna@mail.ru>
2021-04-13 19:22:12 +0600

    refactor: get-all-words

--- src/utils/getAllWordsCurrPrevPages.js
+++ src/utils/getAllWordsCurrPrevPages.js
@@ -1,18 +1,21 @@
 import getWords from '../api/getWords';
 
-const allWordsCurrPrevPagesArray = [];
-
-const getAllWordsCurrPrevPages = async (group = 0, page = 0) => {
+const getTheWords = async (group = 0, page = 0, allWordsArray) => {
   let currPage = page;
   if (currPage >= 0) {
     const data = await getWords(group, page);
-    allWordsCurrPrevPagesArray.push(data);
+    allWordsArray.push(data);
     if (currPage > 0) {
       currPage -= 1;
-      await getAllWordsCurrPrevPages(group, currPage);
+      await getTheWords(group, currPage, allWordsArray);
     }
   }
-  return allWordsCurrPrevPagesArray;
+  return allWordsArray;
+};
+
+const getAllWordsCurrPrevPages = async (group = 0, page = 0) => {
+  const resWordsArray = await getTheWords(group, page, []);
+  return resWordsArray;
 };
 
 export default getAllWordsCurrPrevPages;
