8993000bd87e81e3e3daba138a678b03bf7c1048
Velidoss <godofthemountain09@gmail.com>
2021-04-14 19:04:41 +0300

    feat: added styling for savanna

--- src/components/MiniGames/Savannah/SavannahControl/SavannahControl.jsx
+++ src/components/MiniGames/Savannah/SavannahControl/SavannahControl.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles, Container } from '@material-ui/core';
 import savannahConstants from '../../../../constants/savannahConstants';
 import SavannahStart from '../SavannahStart/SavannahStart';
 import {
@@ -13,12 +14,29 @@ import { setWordGameStatistics } from '../../../../store/textBookReducer/userWor
 import { useAuth } from '../../../../contexts/AuthContext';
 import userWordsConstants from '../../../../constants/userWordsConstants';
 
+const useStyles = makeStyles(() => ({
+  sprintResultP: {
+    textAlign: 'center',
+    margin: '1rem',
+  },
+  wrapperContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '60vh',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '2rem auto',
+  },
+}));
+
 const { WORD_ANSWER_RIGHT, WORD_ANSWER_WRONG } = userWordsConstants;
 
 const SavannahControl = () => {
   const dispatch = useDispatch();
   const { auth: { token, userId }, isAuth } = useAuth();
   const state = useSelector(savannahSelector);
+
+  const classes = useStyles();
 
   const { GAME_STATE_START, GAME_STATE_ACTIVE, GAME_STATE_RESULT } = savannahConstants.gameStates;
 
@@ -56,25 +74,39 @@ const SavannahControl = () => {
 
   switch (state.gameState) {
     case GAME_STATE_START:
-      return <SavannahStart startGame={startGame} />;
+      return (
+        <Container className={classes.wrapperContainer}>
+          <SavannahStart startGame={startGame} />
+        </Container>
+      );
     case GAME_STATE_ACTIVE:
       return (
-        <SavannahActive
-          makeAnswer={makeAnswer}
-          words={state.words}
-          finishGame={finishGame}
-        />
+        <Container className={classes.wrapperContainer}>
+          <SavannahActive
+            makeAnswer={makeAnswer}
+            words={state.words}
+            finishGame={finishGame}
+          />
+        </Container>
+
       );
     case GAME_STATE_RESULT:
       return (
-        <SavannahResult
-          eraseGameState={() => dispatch(eraseGameState())}
-          right={state.rightAnswers}
-          wrong={state.wrongAnswers}
-        />
+        <Container className={classes.wrapperContainer}>
+          <SavannahResult
+            eraseGameState={() => dispatch(eraseGameState())}
+            right={state.rightAnswers}
+            wrong={state.wrongAnswers}
+          />
+        </Container>
+
       );
     default:
-      return <SavannahStart startGame={startGame} />;
+      return (
+        <Container className={classes.wrapperContainer}>
+          <SavannahStart startGame={startGame} />
+        </Container>
+      );
   }
 };
 
