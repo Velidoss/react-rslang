4d2c01f8365a7cce84d4a6f59f95b264c85523de
Irina Stepanova <irina.s.vesna@mail.ru>
2021-04-16 15:21:34 +0600

    feat: add choose-level to mobile menu

--- src/components/Header/HeaderNav/NavDrawer/NavDrawer.style.js
+++ src/components/Header/HeaderNav/NavDrawer/NavDrawer.style.js
@@ -10,4 +10,14 @@ export default makeStyles((theme) => ({
   nested: {
     paddingLeft: theme.spacing(2),
   },
+  modalContainer: {
+    width: '380px',
+    backgroundColor: theme.palette.type === 'dark'
+      ? '#43373D'
+      : '#F6F4F5',
+    padding: '1rem',
+    margin: '2rem auto',
+    borderRadius: '0.5rem',
+    textAlign: 'center',
+  },
 }));
