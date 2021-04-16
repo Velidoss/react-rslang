63b0dc71392f3649e7db28af3c88465114d60dd4
Irina Stepanova <irina.s.vesna@mail.ru>
2021-04-16 13:08:49 +0600

    feat: add error handling to get-words

--- src/api/getWords.js
+++ src/api/getWords.js
@@ -2,8 +2,11 @@ import createInstance from './createInstance';
 
 const getWords = async (group = 0, page = 0) => {
   const request = createInstance('/words', { group, page });
-  const response = await request.get();
-  return response.data;
+  const response = await request.get().catch((error) => {
+    console.log(error);
+    return {};
+  });
+  return response.data || [];
 };
 
 export default getWords;
